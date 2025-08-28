97de107f21fd11f6a2469683177e3c2bd63a7dc2
HashenWatawala <hashanewatawala@gmail.com>
2025-08-28 09:16:59 +0530

    Navbar linked

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
-import Header3 from './components/Navbar'
-import TriangleBackgroundView from './components/Home'
-import Header from './components/Navbar'
-import About from './components/About'
-import PdfUploader from './components/PDFuploader'
-import ContactPage from './components/ContactPage'
-import Footer2 from './components/Footer'
-
+import React from 'react';
+import Header from './components/Navbar';
+import TriangleBackgroundView from './components/Home';
+import About from './components/About';
+import PdfUploader from './components/PDFuploader';
+import ContactPage from './components/ContactPage';
+import Footer2 from './components/Footer';
 
 const App = () => {
   return (
     <div>
-      <Header/>
-      <div> <TriangleBackgroundView/></div>
-     <div><About/></div>
-     <div>
-      <PdfUploader />
-    </div>
-    <div><ContactPage/></div>
-    <Footer2/>
-    </div>
-  )
-}
+      <Header />
 
-export default App
+      <section id="home">
+        <TriangleBackgroundView />
+      </section>
+
+      <section id="about">
+        <About />
+      </section>
+
+      <section id="upload">
+        <PdfUploader />
+      </section>
+
+      <section id="contact">
+        <ContactPage />
+      </section>
+
+      <Footer2 />
+    </div>
+  );
+};
+
+export default App;

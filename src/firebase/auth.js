c25b2d5de1928588e9917b5ef829228478f9cc29
HashenWatawala <hashanewatawala@gmail.com>
2025-08-30 21:42:40 +0530

    Functio created

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,6 +1,35 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, updatePassword} from "firebase/auth";
 import {auth} from "./firebase";
 
 export const doCreateUserWithEmailAndPassword = async (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignWithUserWithEmailAndPassword = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInWithGoogle = async()=>{
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+
+    return result
+};
+
+export const doSignOut = () =>{
+    return auth.signOut();
+};
+
+export const doPasswordReset = (email) =>{
+    return sendPasswordResetEmail(auth, email);
+};
+
+export const doPasswordChange = (password) =>{
+    return updatePassword(auth.currentUser, password);
+};
+
+export const doSendEmailVerfication = ()=>{
+    return sendEmailVerification(auth.currentUser, {
+        url: `${window.location.origin}/home`,
+    });
 };
